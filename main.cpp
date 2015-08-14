7256e6de507ec7b809b3564be923ff2fcd3b980c
Eswar Dhinak <eswardhinak@Eswars-MacBook-Pro.local>
2015-08-13 23:41:58 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/**
+	Basic Stream Cipher and Vigener Cipher Encryption of Source Code. 
+**/
+
+#include <iostream>
+#include <stat>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char ** argv){
+	cout << "Starting program..." << endl;
+	cout << "(e)ncrypt or (d)ecrypt";
+	char choice;
+	cin >> choice;							//encryption mode vs decryption mode
+
+	cout << "Enter path:  ";
+	string path;
+	cin >> path; 							//get path to source code
+
+	
+
+
+
+
+
+
+}
